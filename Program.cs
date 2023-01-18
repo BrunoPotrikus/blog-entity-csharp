331e96903707e7eddfd510561cf96207ccce91ad
RafaelBortolozoLuxSistemas <rafaelbortolozo@luxsistemas.com.br>
2023-01-18 15:28:36 -0300

    Aplicação utilizando Entity Framework

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using BlogEntity.Data;
+using BlogEntity.Models;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        using(var context = new BlogDataContext())
+        {
+            //Create (INSERT)
+            //var tag = new Tag { Name="ASP.NET", Slug="aspnet" };
+            //context.Tags.Add(tag);
+            //context.SaveChanges();
+
+            //UPDATE
+            //var tag = context.Tags.FirstOrDefault(x => x.Id == 3);
+            //tag.Name = ".NET";
+            //tag.Slug = "dotnet";
+            //context.Update(tag);
+            //context.SaveChanges();
+
+            //DELETE
+            //var tag = context.Tags.FirstOrDefault(x => x.Id == 3);
+            //context.Remove(tag);
+            //context.SaveChanges();
+
+            //Read (SELECT)
+
+        }
+    }
+}
